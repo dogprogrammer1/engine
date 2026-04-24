5764199a0fb5bf12b3291eb4f52f4e9481dcc247
Rondonuwu <h.rondonuwu29@bcp.org>
2026-04-24 13:30:59 -0700

    feat: extract selection and board creation from main.js to game.js

---
+++ game.js
@@ -0,0 +1,38 @@
+import Board from "./board.js";
+
+export default class Game {
+    constructor(renderer) {
+        this.board = new Board();
+        this.renderer = renderer;
+        this.selected = false;
+        this.selX = -1;
+        this.selY = -1;
+    }
+
+    click(x, y) {
+        if(!this.selected){
+            if(this.board.getPiece(x,y)[1] !== -1){
+                this.selected = true;
+                this.selX = x;
+                this.selY = y;
+            }
+        }else{
+            this.board.move(this.selX, this.selY, x, y);
+            this.selected = false;
+        }
+
+        this.draw();
+    }
+
+    selection() {
+        return {
+            selected: this.selected,
+            x: this.selX,
+            y: this.selY
+        };
+    }
+
+    draw() {
+        this.renderer.draw(this.board, this.selection());
+    }
+}
--- main.js
+++ main.js
@@ -1,47 +1,14 @@
-import Board from "./board.js";
-import { drawPieces } from "./renderer.js";
-
-let b = new Board();
-
+import Game from "./game.js";
+import Renderer from "./renderer.js";
 
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.height = 480;
-canvas.width = 480;
-
-let selected = false;
-let selX = -1;
-let selY = -1;
-
-function drawBoard(){
-    for(let y=0;y<8;y++){
-        for(let x=0;x<8;x++){
-            ctx.fillStyle = (x+y)%2===0 ? "#eee" : "#057a14";
-            if(selected && x===selX && y===selY){
-                ctx.fillStyle = "rgb(84, 96, 223)";
-            }
-            ctx.fillRect(x*60,y*60,60,60);
-        }
-    }
-    drawPieces(ctx, b);
-}
-
+const renderer = new Renderer(canvas);
+const game = new Game(renderer);
 
 canvas.addEventListener("click", e => {
     let x = Math.floor(e.offsetX / 60);
     let y = Math.floor(e.offsetY / 60);
-    if(!selected){
-        if(b.getPiece(x,y)[1] !== -1){
-            selected = true;
-            selX = x;
-            selY = y;
-        }
-    }else{
-        b.move(selX, selY, x, y);
-        selected = false;
-    }
-    drawBoard();
+    game.click(x, y);
 });
-drawBoard();
+game.draw();
 
