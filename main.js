c7f77e246725ffa880600fd4aef495fa86b2a790
Hudson <hrondonuwu@gmail.com>
2026-04-25 17:27:36 -0700

    refactor: extract a bunch of hardcoded UI variables to ui-constants.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import Game from "./game.js";
 import Renderer from "./renderer.js";
+import { SQUARE_SIZE } from "./ui-constants.js";
 
 const canvas = document.getElementById("canvas");
 const renderer = new Renderer(canvas);
 const game = new Game(renderer);
 
 canvas.addEventListener("click", e => {
-    let x = Math.floor(e.offsetX / 60);
-    let y = Math.floor(e.offsetY / 60);
+    let x = Math.floor(e.offsetX / SQUARE_SIZE);
+    let y = Math.floor(e.offsetY / SQUARE_SIZE);
     game.click(x, y);
 });
 game.draw();
-
--- renderer.js
+++ renderer.js
@@ -1,20 +1,30 @@
+import {
+    BOARD_SIZE,
+    CANVAS_SIZE,
+    PIECE_FONT,
+    PIECE_OFFSET_X,
+    PIECE_OFFSET_Y,
+    PIECE_SYMBOLS,
+    SQUARE_SIZE
+} from "./ui-constants.js";
+
 export default class Renderer {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
 
-        canvas.height = 480;
-        canvas.width = 480;
+        canvas.height = CANVAS_SIZE;
+        canvas.width = CANVAS_SIZE;
     }
 
     draw(board, selection) {
-        for(let y=0;y<8;y++){
-            for(let x=0;x<8;x++){
+        for(let y=0;y<BOARD_SIZE;y++){
+            for(let x=0;x<BOARD_SIZE;x++){
                 this.ctx.fillStyle = (x+y)%2===0 ? "#eee" : "#057a14";
                 if(selection.selected && x===selection.x && y===selection.y){
                     this.ctx.fillStyle = "rgb(84, 96, 223)";
                 }
-                this.ctx.fillRect(x*60,y*60,60,60);
+                this.ctx.fillRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
             }
         }
 
@@ -24,15 +34,12 @@ export default class Renderer {
     drawPieces(board) {
         for(const piece of board.getPieces()){
             this.ctx.fillStyle = piece.color===0 ? "#333" : "#aaa";
-            this.ctx.font = "40px Arial";
-            let symbol = "";
-            if(piece.type===0) symbol = "P";
-            if(piece.type===1) symbol = "B";
-            if(piece.type===2) symbol = "N";
-            if(piece.type===3) symbol = "R";
-            if(piece.type===4) symbol = "Q";
-            if(piece.type===5) symbol = "K";
-            this.ctx.fillText(symbol, piece.x*60+15, piece.y*60+45);
+            this.ctx.font = PIECE_FONT;
+            this.ctx.fillText(
+                PIECE_SYMBOLS[piece.type],
+                piece.x * SQUARE_SIZE + PIECE_OFFSET_X,
+                piece.y * SQUARE_SIZE + PIECE_OFFSET_Y
+            );
         }
     }
 }
